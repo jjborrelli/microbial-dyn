3006b345e151ae379bf630f5a193918677309b0a
Jon Borrelli <borrejj@gmail.com>
2016-09-08 16:18:31 -0400

    script to rep stein

---
+++ RScripts/replicate.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+
+stOTU <- as.data.frame(t(read.csv("Data/stein-OTUcount.csv", header = F, row.names = 1)))
+head(stOTU)
+table(stOTU[stOTU$Population == 2,2])
+
+matplot(select(stOTU, undefined_genus_of_Enterobacteriaceae:Other), typ = "l")
+
+otuCOUNT <- select(stOTU, undefined_genus_of_Enterobacteriaceae:Other)
+barplot(t(otuCOUNT[stOTU$Population == 1 & stOTU$Replicate == 1,])~factor(stOTU[stOTU$Population == 1 & stOTU$Replicate == 1,4]))
+?barplot
+
+
+stOTU[stOTU$Population == 1 & stOTU$Replicate == 1,]
+
+res1 <- ode(unlist(otuCOUNT[stOTU$Population == 1 & stOTU$Replicate == 1,][1,]), c(0,2,6,13), parms = parms, func = lvmod2, events = list(func = ext1, time =  c(0,2,6,13)))
+
+times <- stOTU[stOTU$Population == 1 & stOTU$Replicate == 3,4]
+barplot(t(res1[res1[,1] %in% times, -1]))
+
+ggplot(melt(res1[,-1]), aes(x = Var1, y = value, fill = Var2)) + geom_area(position = "stack")
+
+
+table(stOTU$Population)
+table(stOTU$Replicate)
+combos <- unique(cbind(stOTU$Population, stOTU$Replicate))
+par(mfrow = c(3,3))
+for(i in 1:9){
+  barplot(t(otuCOUNT[stOTU$Population == combos[i,1] & stOTU$Replicate == combos[i,2],]), main = paste(combos[i,], sep = ","))
+}
+
+res1 <- list()
+for(i in 1:9){
+  res1[[i]] <- ode(unlist(otuCOUNT[stOTU$Population == combos[i,1] & stOTU$Replicate == combos[i,2],][1,]), 0:30, parms = parms, func = lvmod2, events = list(func = ext1, time =  1:30))
+  matplot(res1[[i]][,-1], typ = "l", lwd = 2)
+}
+
+res2 <- lapply(res1, function(x) x[,-1])
+res3 <- lapply(res2, function(x) t(apply(x, 1, function(y) y/sum(y))))
+ggplot(melt(res3), aes(x = Var1, y = value, fill = Var2)) + geom_area(position = "stack") + facet_wrap(~L1)
