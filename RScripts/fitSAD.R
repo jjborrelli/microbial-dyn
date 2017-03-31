9e513e3ad56478b4063ee44a2980b2a24d724fba
Jon Borrelli <borrejj@gmail.com>
2017-03-31 11:46:19 -0400

    add functions for data gathering and prep

--- RScripts/fitSAD.R
+++ RScripts/fitSAD.R
@@ -123,15 +123,49 @@ plot(t(sapply(fzotu, function(x) x@fullcoef)))
 ##################################################################
 ### Read in data
 
+get_dat <- function(fpath, connected = TRUE){
+  lf1 <- list.files(fpath)
+  lf2 <- grep("ge", lf1)
+  lf3 <- grep("mat", lf1)
+  
+  eqmat <- list()
+  iconn <- c()
+  for(i in 1:length(lf2)){
+    ge1 <- readRDS(paste(fpath, lf1[lf2][[i]], sep = ""))
+    if(any(is.na(ge1))){next}
+    if(any(is.na(ge1$eqst))){next}
+    
+    mat1 <- readRDS(paste(filepath1, lf1[lf3][[i]], sep = ""))
+    
+    iconn[i] <- is.connected(graph.adjacency(abs(sign(mat1[ge1$spp, ge1$spp]))))
+    
+    eqmat[[i]] <- data.frame(itystr(mat1[ge1$spp, ge1$spp]), web = i, N = sum(ge1$spp))
+    eqabs[[i]] <- sort(ge1$eqst, decreasing = T)
+    
+    cat(round(i/length(lf2)), "--:::--")
+  }
+  
+  eqa <- eqabs[!is.na(eqabs) & !sapply(eqabs, is.null)]
+  eqm <- eqmat[!is.na(eqabs) & !sapply(eqabs, is.null)]
+  
+  if(connected){
+    eqa <- eqa[iconn[!is.na(eqabs) & !sapply(eqabs, is.null)]]
+    eqm <- eqm[iconn[!is.na(eqabs) & !sapply(eqabs, is.null)]]
+  }
+  
+  return(list(eqa = eqa, eqm = eqm))
+}
+
+
 ## File path location of the data
-filepath1 <- "~/Documents/Data/parSADhub_data/"
+filepath1 <- "~/Documents/Data/parSAD_data2/"
 lf1 <- list.files(filepath1)
 lf2 <- grep("ge", lf1)
 lf3 <- grep("mat", lf1)
 
 
 # Get equilibrium community interaction matrices and convert into df of interaction numbers and strengths
-eqmat2 <- list()
+eqmat3 <- list()
 iconn2 <- c()
 for(i in 1:length(lf2)){
   ge1 <- readRDS(paste(filepath1, lf1[lf2][[i]], sep = ""))
@@ -141,18 +175,18 @@ for(i in 1:length(lf2)){
   
   iconn2[i] <- is.connected(graph.adjacency(abs(sign(mat1[ge1$spp, ge1$spp]))))
   
-  eqmat2[[i]] <- data.frame(itystr(mat1[ge1$spp, ge1$spp]), web = i, N = sum(ge1$spp))
+  eqmat3[[i]] <- data.frame(itystr(mat1[ge1$spp, ge1$spp]), web = i, N = sum(ge1$spp))
   print(i)
 }
 
 # Get equilibrium abundances in a list
-eqabs2 <- list() #matrix(0, nrow = 100, ncol = 1000)
+eqabs3 <- list() #matrix(0, nrow = 100, ncol = 1000)
 for(i in 1:length(lf2)){
   ge1 <- readRDS(paste(filepath1, lf1[lf2][[i]], sep = ""))
   if(any(is.na(ge1))){eqabs[[i]] <- NA; next}
   if(any(is.na(ge1$eqst))){eqabs[[i]] <- NA;next}
   
-  eqabs2[[i]] <- sort(ge1$eqst, decreasing = T)
+  eqabs3[[i]] <- sort(ge1$eqst, decreasing = T)
   print(i)
 }
 
@@ -174,12 +208,20 @@ simfzhub <- lapply(eqabs2[!sapply(eqabs2, is.null)], function(x){x <- x[x > 0]; 
 simR2hub <- sapply(1:length(eqabs2[!sapply(eqabs2, is.null)]), function(x) r2modified(sort(eqabs2[!sapply(eqabs2, is.null)][[x]][eqabs2[!sapply(eqabs2, is.null)][[x]] > 0],decreasing = T), radpred(simfzhub[[x]])$abund))
 hist(simR2hub)
 
+# Fit zipf RAD to random communities with varying pars
+sim2fz <- lapply(eqabs3[!sapply(eqabs3, is.null)], function(x){x <- x[x >0]; fitzipf_r(x)})
+plot(t(sapply(sim2fz, function(x) x@fullcoef)))    
+## Get R squared
+sim2R2 <- sapply(1:length(eqabs3[!sapply(eqabs3, is.null)]), function(x){r2modified(sort(eqabs3[!sapply(eqabs3, is.null)][[x]][eqabs3[!sapply(eqabs3, is.null)][[x]] > 0],decreasing = T), radpred(sim2fz[[x]])$abund)})
+
 
 # Compute connectance of equilibrium matrices
 ## Randoms
 conn1 <- sapply(eqmat[!is.na(eqabs)], function(x) sum(x$num)/(x$N[1] *x$N[1]))
 ## Hubs
 conn2 <- sapply(eqmat2[!is.na(eqabs2)], function(x) sum(x$num)/(x$N[1] *x$N[1]))
+## Randoms with diff pars
+conn3 <- sapply(eqmat3[!sapply(eqabs3, is.null)], function(x) sum(x$num)/(x$N[1] *x$N[1]))
 ## Plotting fitted pars against connectance 
 plot(conn1[!is.na(eqabs)], sapply(simfz, function(x) x@coef))
 plot(conn2, sapply(simfzhub, function(x) x@coef)[!is.na(eqabs2)])
@@ -187,11 +229,13 @@ plot(conn2, sapply(simfzhub, function(x) x@coef)[!is.na(eqabs2)])
 
 # Get vector of fitted par (s) for random dat
 s.val <- sapply(simfz, function(x) x@coef)
-
+s.val2 <- sapply(simfzhub, function(x) x@coef)
+s.val3 <- sapply(sim2fz, function(x) x@coef)
 # Get matrix of all fitted pars for random, hub, and real comms
 fitpars <- rbind(cbind(t(sapply(simfz, function(x) x@fullcoef)),typ = 1, r2 = simR2),
                  cbind(t(sapply(simfzhub, function(x) x@fullcoef)),typ = 2, r2 = simR2hub),
-                 cbind(t(sapply(fzotu, function(x) x@fullcoef)), typ = 3, r2 = otuR2))
+                 cbind(t(sapply(fzotu, function(x) x@fullcoef)), typ = 3, r2 = otuR2),
+                 cbind(t(sapply(sim2fz, function(x) x@fullcoef)), typ = 4, r2 = sim2R2))
 fitpars[,"r2"][fitpars[,"r2"] < 0]  <- 0   ## make any negative rsquared 0
 
 # plot relationship between comm size and s
@@ -203,10 +247,46 @@ ggplot(data.frame(fitpars), aes(x = N, y = s, col = factor(typ), alpha = r2)) + 
 ##################################################################
 ##################################################################
 ### Interactions
+#testing objs
+eqa <- eqabs3[!sapply(eqabs3, is.null)]
+eqm <- eqmat3[!sapply(eqabs3, is.null)]
+prepdat <- function(eqa, eqm, svals, sr2){
+  Nspp <- sapply(eqa, length)
+  conn <- sapply(eqm, function(x) sum(x$num)/(x$N[1] *x$N[1]))
+  # pull out interaction numbers
+  int1 <- t(sapply(eqm, function(x){if(is.null(x)){return(c(NA,NA,NA,NA,NA))};x$num}))
+  istrs <- lapply(eqm, function(x){x$m1[2] <- x$m2[2]; x$m2[2] <- 0;return(x)}) ## move main commensal strength with others
+  # pull out interaction strengths
+  int3 <- t(sapply(istrs, function(x){if(any(is.na(unlist(x)))){return(c(NA,NA,NA,NA,NA))};x$m1}))
+  # get positive predation strength 
+  p2 <- sapply(istrs, function(x) x$m2[5])
+  # put interaction strengths into one matrix
+  allint <- cbind(int1, int3, p2)
+  colnames(allint) <- c("aN", "coN", "cpN", "mN", "pN", "aS", "coS", "cpS", "mS", "pSn", "pSp")
+  # put ints and fitted par into one dataframe
+  dat <- data.frame(sV = svals, sR = sr2, allint, Nsp = Nspp, C = conn)
+  
+  return(dat)
+}
 
+pdat <- prepdat(eqa = eqa, eqm = eqm, svals = s.val3, sr2 = sim2R2)
+fit.init <- (lm(sV~Nsp+C+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = pdat[iconn2[!sapply(eqabs3, is.null)],], na.action = "na.fail"))
+fit.init.ints <- (lm(sV~Nsp+C+aN*aS+coN*coS+cpN*cpS+mN*mS+pN*pSn+pSp+pN:pSp, data = pdat[iconn2[!sapply(eqabs3, is.null)],], na.action = "na.fail"))
+summary(fit.init.ints)
+
+head(MuMIn::dredge(fit.init.ints))
+predict(fit.init, newdata = pdat[iconn2[!sapply(eqabs3, is.null)],][100,])
+pdat$sV[iconn2[!sapply(eqabs3, is.null)]][100]
+
+#fitlinmod <- function(pdat, r2cutoff = 0.5, boot = F){
+#  fit.init <- (lm(sV~Nsp+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = pdat))
+#  summary(fit.init)
+#}
+
+Nspp <- sapply(eqabs[!is.na(eqabs)], length)
 # pull out interaction numbers
 int1 <- t(sapply(eqmat[!is.na(eqabs)], function(x){if(is.null(x)){return(c(NA,NA,NA,NA,NA))};x$num}))
-istrs <- lapply(eqmat, function(x){x$m1[2] <- x$m2[2]; x$m2[2] <- 0;return(x)}) ## move main commensal strength with others
+istrs <- lapply(eqmat, function(x){x$m1[2] <- x$m2[2]; x$m2[2] <- 0;x$m1[4] <- x$m2[4]; x$m2[4] <- 0;return(x)}) ## move main commensal strength with others
 # pull out interaction strengths
 int3 <- t(sapply(istrs[!is.na(eqabs)], function(x){if(any(is.na(unlist(x)))){return(c(NA,NA,NA,NA,NA))};x$m1}))
 # get positive predation strength 
@@ -215,7 +295,7 @@ p2 <- sapply(istrs[!is.na(eqabs)], function(x) x$m2[5])
 allint <- cbind(int1, int3, p2)
 colnames(allint) <- c("aN", "coN", "cpN", "mN", "pN", "aS", "coS", "cpS", "mS", "pSn", "pSp")
 # put ints and fitted par into one dataframe
-dat <- data.frame(sR = simR2, allint)
+dat <- data.frame(sR = simR2, allint, Nsp = Nspp)
 # rescale by column means (units of 2 standard deviations)
 dat2 <- apply(dat, 2, function(x) (x - mean(x))/(2*sd(x)))
 dat2[,1] <- dat$sR
@@ -223,21 +303,21 @@ dat2[,1] <- dat$sR
 
 # fit linear models
 ## to scaled data
-summary(lm(sR~aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = data.frame(dat2[dat2[,1] > 0,])))
+summary(lm(sR~Nsp+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = data.frame(dat2[dat2[,1] > 0,])))
 ## to original data, but only for those where zipf has good fit
-fit.init <- (lm(sR~aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = dat[dat$sR > .5,]))
+fit.init <- (lm(sR~Nsp+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = dat[dat$sR > .5,]))
 summary(fit.init)
 
 ## subset original data
 mydat <- dat[dat$sR > .5,]
 
 ## bootstrapping confidence intervals for linmod coefficients
-coefs.bs <- matrix(nrow = 200, ncol = 12)
+coefs.bs <- matrix(nrow = 200, ncol = 13)
 colnames(coefs.bs) <- names(coefficients(fit.init))
 r2val.bs <- c()
 for(i in 1:200){
   bs.rows <- sample(1:nrow(mydat), nrow(mydat), replace = T)
-  fit.bs <- lm(sR~aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = mydat[bs.rows,])
+  fit.bs <- lm(sR~Nsp+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = mydat[bs.rows,])
   
   coefs.bs[i,] <- coefficients(fit.bs)
   r2val.bs[i] <- summary(fit.bs)$r.squared
@@ -248,8 +328,8 @@ apply(coefs.bs, 2, function(x) quantile(x, probs = c(0.025, 0.975)))
 #MuMIn::dredge(lm(sR~aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = dat[dat$sR > .5,], na.action = "na.fail"))
 
 ## Check model for how interactions affect fitting of the zipf (rsquared)
-summary(glm(as.numeric(simR2 < 0.75)~allint, family = "binomial"))
-DAAG::CVbinary(glm(sR~aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pS, data = dat, family = "binomial"))
+summary(glm(as.numeric(simR2 < 0.5)~allint, family = "binomial"))
+DAAG::CVbinary(glm(sR~Nsp+aN+coN+cpN+mN+pN+aS+coS+cpS+mS+pSn+pSp, data = dat, family = "binomial"))
 
 
 
