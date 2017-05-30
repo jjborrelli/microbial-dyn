d5d6da761649383a30dac20845a162208eb1e246
Jon Borrelli <borrejj@gmail.com>
2017-05-30 16:00:51 -0400

    add mixed and multilevel models

--- RScripts/abund-mod.R
+++ RScripts/abund-mod.R
@@ -27,11 +27,17 @@ dat2.fin$rabi <- unlist(lapply(lapply(dat2, "[[", 2), function(x) x$ab.i/sum(x$a
 
 
 datrel <- rbindlist(lapply(lapply(dat, "[[", 2), function(x){for(i in 1:ncol(x)){x[,i] <- x[,i]/sum(x[,i]); x[,i][is.nan(x[,i])] <- 0};return(x)}))
+datrel$com <- dat.fin$com
+
+datz <- rbindlist(lapply(lapply(dat, "[[", 2), function(x){for(i in 1:ncol(x)){x[,i] <- (x[,i]-mean(x[,i]))/sd(x[,i]); x[,i][is.nan(x[,i])] <- 0};return(x)}))
+datz$com <- dat.fin$com
+datz$ab <- dat.fin$ab
+datz$rab <- dat.fin$rab
 dat2rel <- rbindlist(lapply(lapply(dat2, "[[", 2), function(x){for(i in 1:ncol(x)){x[,i] <- x[,i]/sum(x[,i]); x[,i][is.nan(x[,i])] <- 0};return(x)}))
 
 plot(dat.init$spp~dat.init$ab.i)
-
-fit1 <- lm((ab)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin, x = F, y = F, model = F, na.action = "na.fail")
+library(lme4)
+fit1 <- lmer(rab~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut+(1 | com), data = datz, na.action = "na.fail")
 summary(fit1)
 #dfit1 <- MuMIn::dredge(fit1)
 
@@ -46,7 +52,7 @@ summary(fit3)
 boxplot(predict.glm(fit3, dat.init[s1,], type = "response")~dat.init$spp[s1])
 
 
-maxab <- sapply(lapply(dat,"[[", 2), function(x) max(x$ab))
+maxab <- sapply(lapply(dat,"[[", 2), function(x) max(x$ab/sum(x$ab)))
 summary(maxab)
 maxab2 <- sapply(lapply(dat,"[[", 2), function(x) max(x$ab[-which.max(x$ab)]))
 
@@ -55,7 +61,7 @@ df1 <- as.data.frame(t(sapply(lapply(dat,"[[", 2), function(x) colMeans(x))))
 df1$N <- sapply(lapply(dat,"[[", 2), function(x) nrow(x))
 df1$ma <- maxab
 
-fit2 <- lm(log10(ma)~log10(N)+K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = df1, x = F, y = F, model = F, na.action = "na.fail")
+fit2 <- lm(cbind(ma, N)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = df1, x = F, y = F, model = F, na.action = "na.fail")
 summary(fit2)
 sapply(lapply(dat,"[[", 2), function(x) order(x$ab, decreasing = T))
 
@@ -78,21 +84,68 @@ dat.fin$t10 <- topten
 
 gt3 <- glm(dat.fin$t5~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, family = "binomial", data = dat.fin, x = F, y = F, model = F, na.action = "na.fail")
 summary(gt3)
-DAAG::cv.binary(gt3)
+
 gt5 <- glm(dat.fin$t5~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, family = "binomial", data = dat.fin, x = F, y = F, model = F, na.action = "na.fail")
 summary(gt5)
-DAAG::cv.binary(gt5)
+
 gt10 <- glm(dat.fin$t10~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, family = "binomial", data = dat.fin, x = F, y = F, model = F, na.action = "na.fail")
 summary(gt10)
-DAAG::cv.binary(gt10)
 
 library(MASS)
-ldafit3 <- lda(t3~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin)
-ldafit5 <- lda(t3~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin)
-ldafit10 <- lda(t3~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin)
+ldafit3 <- lda(t3~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin, CV = TRUE)
+tab <- table(factor(dat.fin$t3), ldafit3$class)
+conCV1 <- rbind(tab[1, ]/sum(tab[1, ]), tab[2, ]/sum(tab[2, ]))
+dimnames(conCV1) <- list(Actual = c("No", "Yes"), "Predicted (cv)" = c("No","Yes"))
+print(round(conCV1, 3))
+
+ldafit5 <- lda(factor(t5)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin, CV = TRUE)
+tab <- table(factor(dat.fin$t5), ldafit5$class)
+conCV1 <- rbind(tab[1, ]/sum(tab[1, ]), tab[2, ]/sum(tab[2, ]))
+dimnames(conCV1) <- list(Actual = c("No", "Yes"), "Predicted (cv)" = c("No","Yes"))
+print(round(conCV1, 3))
+
+ldafit10 <- lda(factor(t10)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = dat.fin, CV = TRUE)
+tab <- table(factor(dat.fin$t10), ldafit10$class)
+conCV1 <- rbind(tab[1, ]/sum(tab[1, ]), tab[2, ]/sum(tab[2, ]))
+dimnames(conCV1) <- list(Actual = c("No", "Yes"), "Predicted (cv)" = c("No","Yes"))
+print(round(conCV1, 3))
+
+
+cbind(coefficients(ldafit5),coefficients(ldafit3),coefficients(ldafit10))
+
+
+rf3 <- randomForest(factor(t3)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, 
+                    data = datrel, mtry = 15, ntree = 1000)
+
+
+###########################################################################
+###########################################################################
+maxab <- sapply(lapply(dat,"[[", 2), function(x) max(x$ab/sum(x$ab)))
+summary(maxab)
+
+df1 <- as.data.frame(t(sapply(lapply(dat,"[[", 2), function(x) colMeans(x))))
+df2 <- as.data.frame(apply(df1, 2, function(x){(x-mean(x))/sd(x)}))
+df1$N <- sapply(lapply(dat,"[[", 2), function(x) nrow(x))
+df1$ma <- maxab
+df2$N <- sapply(lapply(dat,"[[", 2), function(x) nrow(x))
+df2$ma <- maxab
+df2$fz <- rbfz[,2]
+fit2 <- lm(cbind(ma, N)~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = df2, x = F, y = F, model = F, na.action = "na.fail")
+summary(fit2)
+fit3 <- lm(fz~K2+bet.w+d.tot+cc.w+apl.w.mu+nComp+CompIn+CompOut+nMut+MutIn+MutOut+nPred+PredIn+PredOut+nAmens+AmensIn+AmensOut+nComm+CommIn+CommOut, data = df2, x = F, y = F, model = F, na.action = "na.fail")
+summary(fit3)
+## 
+library(sads)
+fz1 <- lapply(lapply(dat, "[[", 2), function(x) fzmod(x$ab/sum(x$ab)))
+summary(lm(rbfz[,2]~fit2$fitted.values))
+###########################################################################
+###########################################################################
 
 
 #PCoAres <- vegan::capscale(dplyr::select(dat.fin, K2, bet.w, d.tot, cc.w, apl.w.mu, nComp:CommOut)~1, distance = "bray")
 library(ape)
 pc1 <- princomp(dplyr::select(dat.fin, K2, bet.w, d.tot, cc.w, apl.w.mu, nComp:CommOut))
 loadings(pc1)
+
+library(vegan)
+nmds1 <- metaMDS(dplyr::select(dat.fin, K2, bet.w, d.tot, cc.w, apl.w.mu, nComp:CommOut), distance = "bray")
